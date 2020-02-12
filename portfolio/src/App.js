018650d79e5d8b77f40308c5c21e66b9aa2a34cd
bquizza5 <blaine555@gmail.com>
2020-02-12 10:50:08 -0700

    testing analytics

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,8 +3,14 @@ import Main from './components/Main'
 import NavBar from './components/NavBar'
 import Projects from './components/projects'
 import './App.css';
+import ReactGA from 'react-ga';
 
 function App() {
+
+  const trackingId = "UA-158261004-1";
+  ReactGA.initialize(trackingId);
+  ReactGA.pageview('/homepage');
+
   return (
     <div className="App">
       <NavBar />
