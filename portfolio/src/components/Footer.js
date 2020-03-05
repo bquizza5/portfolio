c09737c5e4bad53cb3cde1c267dee8b92f8b872b
bquizza5 <blaine555@gmail.com>
2020-03-05 16:26:41 -0700

    new changes

--- portfolio/src/components/Footer.js
+++ portfolio/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
 return (
     <div style={{height: 50}}>
         <p>
-            Contact Me on <a href={"https://www.linkedin.com/in/blaine-blonquist-0b2b9bb2/"}>LinkedIn</a>
+        <a href={"https://github.com/bquizza5"}>GitHub</a> - <a href={"https://www.linkedin.com/in/blaine-blonquist-0b2b9bb2/"}>LinkedIn</a>
         </p>
     </div>
 )
--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import placeholder from '../images/placeholder.png'
+import pitchBlack from '../images/pitchBlack.png'
+import waterMyPlants from '../images/watermyplants.png'
 
 const Projects = () => {
     const [projects, setProjects] = useState()
@@ -23,29 +25,29 @@ const Projects = () => {
                     return <div></div>
                 })} */}
                 <div className='project'>
-                    <img className='projectImg' src={placeholder}></img>
+                    <img className='projectImg' src={pitchBlack}></img>
                     <div>
-                    <h2>project 1</h2>
+                    <h2>Pitch Black</h2>
                     <p>
-                        description: the project description the project description
-                         the project description the project description the project
-                          description the project description
+                    Pitch Black is a multi-user-dungeon game with a simple GUI.
+                     I played a fundamental part in building the front-end movement
+                      functionality and I assisted in designing the database.
                     </p>
-                    <p>Technologies</p>
-                    <div>links</div>
+                    <p>Django | 0auth2 | React | React-Router | Axios</p>
+                    <div>Repo - Deployment</div>
                     </div>
                 </div>
                 <div className='project'>
-                    <img className='projectImg' src={placeholder}></img>
+                    <img className='projectImg' src={waterMyPlants}></img>
                     <div>
-                    <h2>project 1</h2>
+                    <h2>Water My Plants</h2>
                     <p>
-                        description: the project description the project description
-                         the project description the project description the project
-                          description the project description
+                    Water My Plants is a web app that helps you keep track 
+                    of your plant watering schedule. I participated by building 
+                    controllers, services, and models for our back-end Spring server.
                     </p>
-                    <p>Technologies</p>
-                    <div>links</div>
+                    <p>Java Spring | 0auth2 | Heroku</p>
+                    <div>Repo</div>
                     </div>
                 </div>
                 <div className='project'>
